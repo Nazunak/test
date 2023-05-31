590f4d7d99e4543641d6a89194450afe64cc5cdd
pavlo <work.reiste@gmail.com>
2023-05-31 20:46:49 +0300

    add if

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-let numberOfFilms;
-numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// let numberOfFilms;
+// numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt('Один из последних просмотреных фидьмов?', '' );
+// const b = +prompt('На сколько оцените его', '' );
+// const c = prompt('Один из последних просмотреных фидьмов?', '' );
+// const d = +prompt('На сколько оцените его', '' );
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+let temperatyra = -3;
+
+if (temperatyra > 0) {
+    console.log("teplo")
+} else {
+    console.log("cold")
 };
-
-const a = prompt('Один из последних просмотреных фидьмов?', '' );
-const b = +prompt('На сколько оцените его', '' );
-const c = prompt('Один из последних просмотреных фидьмов?', '' );
-const d = +prompt('На сколько оцените его', '' );
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
