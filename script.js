1bf4e4eca0771ab020d950977973ab11133980c4
pavlo <work.reiste@gmail.com>
2023-05-31 19:53:08 +0300

    add films project

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
+let numberOfFilms;
+numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
+const a = prompt('Один из последних просмотреных фидьмов?', '' );
+const b = +prompt('На сколько оцените его', '' );
+const c = prompt('Один из последних просмотреных фидьмов?', '' );
+const d = +prompt('На сколько оцените его', '' );
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
