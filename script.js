8d6dcc41f9a334f35d07f7be30eff1b785837167
pavlo <osnova.reiste@gmail.com>
2023-06-03 16:48:50 +0300

    add homework

--- script.js
+++ script.js
@@ -27,17 +27,36 @@
 //     console.log("cold")
 // };
 
-let a = '';
-let lenght = 7;
+// let a = '';
+// let lenght = 7;
 
-for (let i = 1; i < lenght; i++) {
+// for (let i = 1; i < lenght; i++) {
 
-    for (let j = 0; j < i; j++) {
-        a += '*';
-    }
-    
-    a += '\n';
-}
+//     for (let j = 0; j < i; j++) {
+//         a += '*';
+//     }
 
-console.log(a);
+//     a += '\n';
+// }
 
+// console.log(a);
+
+// Место для первой задачи
+
+
+// for (let i = 5; i < 11; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 20; i >=10 ; i--) {
+//     if (i === 13) break;
+//     console.log(i);
+// }
+
+// function thirdTask() {
+//     for (let i = 2; i <= 10; i++) {
+//         if (i % 2 === 0) {
+//             console.log(i);
+//         }
+//     }
+// }
