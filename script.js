1f2f2dfd1c3530409af9b9d62baf7a35256af2c9
pavlo <osnova.reiste@gmail.com>
2023-06-20 10:26:21 +0300

    add function

--- script.js
+++ script.js
@@ -41,10 +41,89 @@
 
 // console.log(a);
 
-for (let i = 2; i <= 16; i++) {
-    if (i % 2 === 0) {
-        continue;
-    } else {
-        console.log(i);
-    }
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// for (let i = 5; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 20; i >= 10; i--) {
+//     if (i === 13) break;
+//     console.log(i)
+// }
+
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// const arr = [];
+
+// for (i = 5; i <= 10; i++) {
+//     arr[i - 5] = i;    
+// }
+// console.log(arr);
+// return arr;
+
+// let numberOfFilms = +prompt('skolko posmotreli?');
+
+// let personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// }
+
+
+// for (i = 0; i < 2; i++) {
+//     let a = prompt('odin iz poslednih filmov?');
+//     let b = prompt('ocenite');
+
+//     personalMovieDB.movies[a] = b;
+// }
+
+// console.log(personalMovieDB);
+
+let numberOfFilms = +prompt('skolko posmotreli filmov?', '');
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 }
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt('odin iz poslednih filmov?', '');
+    let b = +prompt('na skolko ocenite ego?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }   
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('malo');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('klassic');
+} else if (personalMovieDB.count >= 30) {
+    console.log('kinoman');
+} else {
+    console.log('error');
+}
+    
+
+
